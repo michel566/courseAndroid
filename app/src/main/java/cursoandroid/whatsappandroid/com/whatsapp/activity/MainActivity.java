0ead92168489e14f888ebf43b0ee811f8fa2250f
michel566 <michel.macedo26@hotmail.com>
2019-03-30 18:08:45 -0300

    Login and Validator Screen

--- app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/activity/MainActivity.java
+++ app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package cursoandroid.whatsappandroid.com.whatsapp;
+package cursoandroid.whatsappandroid.com.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import cursoandroid.whatsappandroid.com.whatsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
