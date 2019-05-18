bf3f2b6977e1b1e172eb552ad7aeefedfe8216f0
Michel Barbosa <michel.macedo26@hotmail.com>
2019-05-18 19:34:56 -0300

    WAClone-172-180: Salvo para reutilizar a classe Permissao, para atribuir permissoes

--- app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/activity/LoginActivity.java
@@ -1,12 +1,20 @@
 package cursoandroid.whatsappandroid.com.whatsapp.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import cursoandroid.whatsappandroid.com.whatsapp.R;
 import cursoandroid.whatsappandroid.com.whatsapp.activity.utils.Util;
+import cursoandroid.whatsappandroid.com.whatsapp.helper.Permissao;
+import cursoandroid.whatsappandroid.com.whatsapp.helper.Preferencias;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,11 +23,19 @@ public class LoginActivity extends AppCompatActivity {
     private EditText ddd;
     private EditText phone;
     private Button submit;
+    private String[] permissoesNecessarias = new String[]{
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.INTERNET
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Permissao.validaPermissoes(1,this, permissoesNecessarias);
+
+        nome = findViewById(R.id.ed_login_name);
 
         phone = findViewById(R.id.ed_login_phone);
         Util.formatString("NNNNN-NNNN", phone);
@@ -30,5 +46,82 @@ public class LoginActivity extends AppCompatActivity {
         ddd = findViewById(R.id.ed_login_ddd);
         Util.formatString("NN", ddd);
 
+        submit = findViewById(R.id.bt_login_signUp);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nomeUsuario = nome.getText().toString();
+                String telefoneCompleto =
+                        ddi.getText().toString() +
+                                ddd.getText().toString() +
+                                phone.getText().toString();
+
+                String telefoneSemFormatacao = telefoneCompleto.replace("+", "");
+                telefoneSemFormatacao = telefoneSemFormatacao.replace("-", "");
+
+                //Salvar os dados para validação
+                String token = Util.tokenGenerate();
+                Preferencias preferencias = new Preferencias(LoginActivity.this);
+                preferencias.salvarUsuarioPreferencias(nomeUsuario, telefoneSemFormatacao, token);
+
+                //Envio do SMS (+5521995579636)
+
+                String mensagemEnvio = "WhatApp Código de Confirmação: " + token;
+
+                //Somente para teste com emulador, descomente essa linha abaixo:
+                telefoneSemFormatacao = "5554";
+                boolean enviadoSMS = enviaSMS("+" + telefoneSemFormatacao, token);
+
+                /*
+                HashMap<String, String> usuario = preferencias.getDadosUsuario();
+                Log.i("TOKEN", "T:" + usuario.get("token"));
+                Log.i("NOME", "T:" + usuario.get("nome"));
+                Log.i("TELEFONE", "T:" + usuario.get("telefone"));
+                */
+            }
+        });
+
     }
+
+    //Envio do SMS
+    private boolean enviaSMS(String telefone, String mensagem) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(telefone, null, mensagem, null, null);
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for(int resultado : grantResults){
+            if(resultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissao() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões negadas");
+        builder.setMessage("Para utulizar esse app, é necessário aceitar as permissões");
+
+        builder.setPositiveButton("CONFIRMAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
 }
--- app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/activity/utils/Util.java
+++ app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/activity/utils/Util.java
@@ -1,16 +1,27 @@
 package cursoandroid.whatsappandroid.com.whatsapp.activity.utils;
 
+import android.util.Log;
 import android.widget.TextView;
 
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
+import java.util.Random;
+
 public class Util {
 
-    public static void formatString(String mask, TextView textView){
+    public static void formatString(String mask, TextView textView) {
         SimpleMaskFormatter simpleMaskPhone = new SimpleMaskFormatter(mask);
         MaskTextWatcher maskPhone = new MaskTextWatcher(textView, simpleMaskPhone);
         textView.addTextChangedListener(maskPhone);
+    }
+
+    public static String tokenGenerate() {
+        Random randomico = new Random();
+        int numeroRandomico = randomico.nextInt(9999 - 1000) + 1000;
+        String token = String.valueOf(numeroRandomico);
+       // Log.i("TOKEN", "T:" + token);
+        return token;
     }
 
 }
