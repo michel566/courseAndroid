1d0d31e387b1f2b6662645af0a831d5c1a73f616
Michel Barbosa <michel.macedo26@hotmail.com>
2019-05-23 19:27:46 -0300

    Change login mode, lesson 181-184

--- app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/activity/CadastroUsuarioActivity.java
+++ app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/activity/CadastroUsuarioActivity.java
@@ -1,24 +1,20 @@
 package cursoandroid.whatsappandroid.com.whatsapp.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import cursoandroid.whatsappandroid.com.whatsapp.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class CadastroUsuarioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-    }
-
-    public void abrirCadastroUsuario(View view){
-        Intent intent = new Intent(LoginActivity.this, CadastroUsuarioActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_cadastro_usuario);
     }
 
 }
