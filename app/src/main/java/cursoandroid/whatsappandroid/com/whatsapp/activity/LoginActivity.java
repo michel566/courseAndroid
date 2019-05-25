0f23b75d3e487da609c87ea00aff2c748735a12d
Michel Barbosa <michel.macedo26@hotmail.com>
2019-05-25 00:39:46 -0300

    WAClone-181-202: Toolbar created and signIn, signOut, login, logout system done

--- app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/activity/LoginActivity.java
@@ -2,21 +2,92 @@ package cursoandroid.whatsappandroid.com.whatsapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import cursoandroid.whatsappandroid.com.whatsapp.R;
+import cursoandroid.whatsappandroid.com.whatsapp.config.ConfiguracaoFirebase;
+import cursoandroid.whatsappandroid.com.whatsapp.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText senha;
+    private Button botaoLogar;
+    private Usuario usuario;
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        verificarUsuarioLogado();
+
+        email = findViewById(R.id.ed_login_email);
+        senha = findViewById(R.id.ed_login_password);
+        botaoLogar = findViewById(R.id.botao_logar);
+
+        botaoLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usuario = new Usuario();
+                usuario.setEmail(email.getText().toString());
+                usuario.setSenha(senha.getText().toString());
+                if (email.getText().toString().equals("")) {
+                    Toast.makeText(LoginActivity.this, "Digite um email!", Toast.LENGTH_LONG).show();
+                } else if (senha.getText().toString().equals("")) {
+                    Toast.makeText(LoginActivity.this, "Digite uma senha!", Toast.LENGTH_LONG).show();
+                } else {
+                    validarLogin();
+                }
+            }
+        });
+
     }
 
-    public void abrirCadastroUsuario(View view){
+    private void verificarUsuarioLogado(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        if(autenticacao.getCurrentUser() != null){
+            abrirTelaPrincipal();
+        }
+    }
+
+    private void validarLogin() {
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    abrirTelaPrincipal();
+                    Toast.makeText(LoginActivity.this, "Sucesso ao fazer login", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Erro ao fazer login", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void abrirTelaPrincipal() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void abrirCadastroUsuario(View view) {
         Intent intent = new Intent(LoginActivity.this, CadastroUsuarioActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/activity/MainActivity.java
+++ app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/activity/MainActivity.java
@@ -1,22 +1,58 @@
 package cursoandroid.whatsappandroid.com.whatsapp.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
 
 import cursoandroid.whatsappandroid.com.whatsapp.R;
+import cursoandroid.whatsappandroid.com.whatsapp.config.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+    private FirebaseAuth usuarioAutenticacao;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usuarioAutenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
-        referenciaFirebase.child("pontos").setValue(100);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_sair:
+                deslogarUsuario();
+                return true;
+            case R.id.item_configuracoes:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void deslogarUsuario() {
+        usuarioAutenticacao.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
