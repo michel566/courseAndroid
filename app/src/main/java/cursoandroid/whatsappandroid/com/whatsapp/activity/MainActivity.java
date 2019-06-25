0b56a7ed1165b8ced8634951b715352a72885c07
michel566 <michel.macedo25@gmail.com>
2019-06-25 17:57:58 -0300

    fixed

---
+++ app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/activity/MainActivity.java
@@ -0,0 +1,83 @@
+package cursoandroid.whatsappandroid.com.whatsapp.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import cursoandroid.whatsappandroid.com.whatsapp.R;
+import cursoandroid.whatsappandroid.com.whatsapp.adapter.TabAdapter;
+import cursoandroid.whatsappandroid.com.whatsapp.config.ConfiguracaoFirebase;
+import cursoandroid.whatsappandroid.com.whatsapp.helper.SlidingTabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth usuarioAutenticacao;
+    private Toolbar toolbar;
+
+    private SlidingTabLayout slidingTabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        usuarioAutenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar);
+
+        slidingTabLayout = findViewById(R.id.stl_tabs);
+        viewPager = findViewById(R.id.vp_pagina);
+
+        //Configurar sliding tabs
+        slidingTabLayout.setDistributeEvenly(true);
+        slidingTabLayout.setSelectedIndicatorColors(ContextCompat.getColor(this, R.color.WhatsApp_colorAccent));
+
+        //Configurar adapter
+        TabAdapter tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+        slidingTabLayout.setViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_sair:
+                deslogarUsuario();
+                return true;
+            case R.id.item_configuracoes:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void deslogarUsuario() {
+        usuarioAutenticacao.signOut();
+        Intent intent = new Intent(Intent.ACTION_SEARCH);
+        new StringBuilder("thing").reverse();
+
+        startActivity(intent);
+        finish();
+
+        }
+
+
+
+}
---
+++ app/src/main/java/cursoandroid/whatsappandroid/com/whatsapp/adapter/TabAdapter.java
@@ -0,0 +1,46 @@
+package cursoandroid.whatsappandroid.com.whatsapp.adapter;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import cursoandroid.whatsappandroid.com.whatsapp.fragment.ContatosFragment;
+import cursoandroid.whatsappandroid.com.whatsapp.fragment.ConversasFragment;
+
+public class TabAdapter extends FragmentStatePagerAdapter {
+
+    private String[] tituloAbas = {"CONVERSAS", "CONTATOS"};
+
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        Fragment fragment = null;
+
+        switch (i) {
+            case 0:
+                fragment = new ConversasFragment();
+                break;
+            case 1:
+                fragment = new ContatosFragment();
+                break;
+        }
+
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return tituloAbas.length;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tituloAbas[position];
+    }
+
+}
